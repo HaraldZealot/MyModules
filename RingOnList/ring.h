18d99359836ffd767b781db0427d588f32cce64d
Harald Zealot <harald_zealot@tut.by>
2013-06-08 18:45:06 +0300

    license notice

--- RingOnList/ring.h
+++ RingOnList/ring.h
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2013 Alaksiej Piotr Stankievič (Alaksei Stankevich)
+ * All rights reserved.
+ * *************************
+ * Author: Alaksiej Stankievič
+ * *************************
+ * module: RingOnList
+ * project: MyModules
+ *
+ * declaration of classes Ring and RingVoid, implementation of Ring
+ * *************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Its copy is also available at the root directory of the project.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef RING_H
 #define RING_H
 
